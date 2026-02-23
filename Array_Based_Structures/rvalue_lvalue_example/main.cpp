1b75ff676647671070de1cd97aefe1934975ee01
Sam Shue <evilrobotfactory@gmail.com>
2026-02-23 17:18:56 -0500

    Added rvalue example

--- Array_Based_Structures/rvalue_lvalue_example/main.cpp
+++ Array_Based_Structures/rvalue_lvalue_example/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int getValue() {
@@ -17,7 +18,10 @@ int main() {
     int&& r1 = 100;      // 100 is an rvalue
     int&& r2 = a + 1;    // (a + 1) is an rvalue
 
-    int&& r3 = std::move(a);  // std::move(a) is an xvalue
+    int&& r1 = 100;
+
+    int&& r2 = r1;        // ERROR
+    int&& r3 = std::move(r1);  // OK
 
     cout << "Done\n";
 }
